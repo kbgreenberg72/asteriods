bef00c2bcae79ead780ce68c4824436b955df2ff
kbgreenberg72 <kyle.greenberg93@gmail.com>
2024-08-27 17:27:52 -0400

    Adding sprite groups

--- main.py
+++ main.py
@@ -5,21 +5,34 @@ from player import Player
 
 def main ():
     pygame.init() # initialize the gmae
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0 # delta time
     
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         # Handle quit event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
+        for item in updatable:
+            item.update(dt)
+        
         screen.fill((0,0,0)) #create a black screen
-        player.draw(screen)
-        player.update(dt)
+        
+        #MUST GO AFTER THE SCREEN FILL 
+        for item in drawable:
+            item.draw(screen)
+        
+
         pygame.display.flip() # refresh the screen
 
         dt = clock.tick(FPS)# keep this at the end of the while loop for FPS control
