9ff8163cc31cf0c67e3d4002cea22c5d906745c1
kbgreenberg72 <kyle.greenberg93@gmail.com>
2024-08-26 20:28:43 -0400

    added the window functionality and a quit ability to the while loop

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ from constants import *
 
 def main ():
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    #print(f"Screen width: {SCREEN_WIDTH}")
+    #print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        # Handle quit event
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill((0,0,0))
+        pygame.display.flip()
     
     
 if __name__ == "__main__":
