b3726c383731f1e524e71487c0a50d3ec8e210b4
kbgreenberg72 <kyle.greenberg93@gmail.com>
2024-08-26 20:34:41 -0400

    Added a frame rate control system

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ def main ():
     print("Starting asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
     #print(f"Screen height: {SCREEN_HEIGHT}")
-    pygame.init()
+    pygame.init() # initialize the gmae
+    
+    clock = pygame.time.Clock()
+    dt = 0 # delta time
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     while True:
@@ -14,9 +18,13 @@ def main ():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill((0,0,0))
-        pygame.display.flip()
-    
+        screen.fill((0,0,0)) #create a black screen
+        pygame.display.flip() # refresh the screen
+        
+        
+        
+        dt = clock.tick(FPS)# keep this at the end of the while loop for FPS control
+        dt /= 1000 # convert to seconds
     
 if __name__ == "__main__":
     main()
